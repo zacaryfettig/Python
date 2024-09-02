07eb2f2ed99124d17ce6ac51ec196c6d9fd409b7
zacaryfettig <115122598+zacaryfettig@users.noreply.github.com>
2024-09-01 22:07:36 -0600

    Update __main__.py

--- Pulumi-Azure-Container-Apps/__main__.py
+++ Pulumi-Azure-Container-Apps/__main__.py
@@ -1,9 +1,7 @@
 """An Azure RM Python Pulumi program"""
 
 import pulumi
-from pulumi_azure_native import storage
 from pulumi_azure_native import resources
-from pulumi_azure_native import containerregistry
 from pulumi_azure_native import operationalinsights
 from pulumi_azure_native import resources
 from pulumi_azure_native import app
